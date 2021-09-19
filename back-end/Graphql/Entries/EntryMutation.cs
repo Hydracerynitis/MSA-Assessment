c78551bd81e956fa221f4f7a5598abd47582a5e3
Hydracerynitis <1578373943@qq.com>
2021-09-19 12:57:16 +1200

    update scheme

--- back-end/Graphql/Entries/EntryMutation.cs
+++ back-end/Graphql/Entries/EntryMutation.cs
@@ -4,6 +4,7 @@ using HotChocolate;
 using HotChocolate.AspNetCore;
 using HotChocolate.AspNetCore.Authorization;
 using HotChocolate.Types;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -45,6 +46,31 @@ namespace back_end.Graphql.Entries
             await context.SaveChangesAsync(cancellationToken);
             return entry;
         }
-        //public async Task<Entry> SubmitEntry(SubmitEntryInput)
+        public async Task<Entry> SubmitEntry(SubmitEntryInput input, ClaimsPrincipal claimsPrincipal, [ScopedService] AppDbContext context, CancellationToken cancellationToken)
+        {
+            var appUserIdStr = claimsPrincipal.Claims.First(c => c.Type == "AppUserId").Value;
+            var destination = await context.Destinations.FirstOrDefaultAsync(e => e.Address == input.Address, cancellationToken );
+            if (destination == null)
+            {
+                destination = new Destination() { Name = input.Name, Address = input.Address };
+                bool interest = false;
+                try
+                {
+                    interest = Boolean.Parse(input.Interest);
+                }
+                finally
+                {
+                    destination.Interest = interest;
+                }
+                context.Destinations.Add(destination);
+            }
+            if (destination.Name != input.Name)
+                destination.Name = input.Name;
+            await context.SaveChangesAsync(cancellationToken);
+            var entry = new Entry() { DayArrive = input.Arrive, DayLeave = input.Leave, AppUserId = int.Parse(appUserIdStr), DestinationId = destination.Id };
+            context.Entries.Add(entry);
+            await context.SaveChangesAsync(cancellationToken);
+            return entry;
+        }
     }
 }
